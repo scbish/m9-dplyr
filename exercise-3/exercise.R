7633216cd80e41d3ee018c9fec09f71c36b32020
Steven Bishop <Steven@D-69-91-177-76.dhcp4.washington.edu>
2016-10-18 15:15:30 -0700

    Completed exercise

--- exercise-3/exercise.R
+++ exercise-3/exercise.R
@@ -9,18 +9,43 @@ install.packages('devtools')
 devtools::install_github("hadley/fueleconomy")
 
 # Require/library the fueleconomy package
-
+library(fueleconomy)
+library(dplyr)
+vehicles <- data.frame(vehicles)
 
 # Which Accura model has the best hwy MPG in 2015? (without method chaining)
-
+make <- filter(vehicles, make == "Acura", year == 2015)
+best <- filter(make, hwy == max(hwy))
+best.model <- select(best, model)
 
 # Which Accura model has the best hwy MPG in 2015? (nesting functions)
-
+best <- filter(filter(vehicles, make == "Acura", year == 2015), hwy == max(hwy))
 
 # Which Accura model has the best hwy MPG in 2015? (pipe operator)
-
+best <- filter(vehicles, make == "Acura", year == 2015) %>% filter(hwy == max(hwy))
 
 ### Bonus ###
 
 # Write 3 functions, one for each approach.  Then, 
 # Test how long it takes to perform each one 1000 times
+NoChain <- function(){
+  make <- filter(vehicles, make == "Acura")
+  year <- filter(make, year == 2015)
+  best <- filter(year, hwy == max(hwy))
+}
+Nesting <- function(){
+  best <- filter(filter(filter(vehicles, make == "Acura"), year == 2015), hwy == max(hwy))
+}
+Pipe <- function(){
+  best <- filter(vehicles, make == "Acura") %>% filter(year == 2015) %>% filter(hwy == max(hwy))
+}
+
+for (i in 1:1000){
+  NoChain()
+}
+for (i in 1:1000){
+  Nesting()
+}
+for (i in 1:1000){
+  Pipe()
+}
